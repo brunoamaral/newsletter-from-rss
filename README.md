950cc91b5d03ede16569e7c99d7be42ebf816a45
Bruno Amaral <mail@brunoamaral.eu>
2024-09-14 17:36:58 +0100

    v0.91 allow sending the email to addresses in a csv file

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 A python script that picks up the latest post from an RSS feed to send it as an email newsletter.
 
-The code finds the email addresses from a subscription manager built with Node-RED and generates the unsubscribe link with data from the server.
+The code finds the email addresses either from a subscription manager built with Node-RED (using an API) or from a CSV file. It generates the unsubscribe link with data from the server.
 
 This would be a v1.0 if the script could work without fetching the addresses from the subscription manager.
 
 ## Assembly required
 
-Current way to fetch email addresses
+### Current way to fetch email addresses
+
+By default, the script fetches email addresses from an API endpoint provided by a subscription manager:
 
 ```python
 # Fetch email addresses
@@ -21,9 +23,23 @@ response = requests.post(url, headers=headers)
 email_addresses = response.text.split(', ')
 ```
 
-If you use Node-RED, you can import the file `nodered__subscription_manager_flow.json` and configure it to match your domain.
+If you use Node-RED, you can import the file `nodered__subscription_manager_flow`.json and configure it to match your domain.
 
-Otherwise you can edit the code to fetch the email addresses from a csv file, for example.
+Alternatively, you can now load email addresses from a CSV file using the --csv argument.
+
+###  Fetching email addresses from a CSV
+
+If you prefer not to use the API, you can supply a CSV file containing the email addresses. The CSV file should have one email address per line.
+
+Example of CSV usage:
+```
+# Load email addresses from a CSV file
+email_addresses = []
+with open(args.csv, newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        email_addresses.append(row[0])
+```
 
 ## Usage
 
@@ -33,16 +49,15 @@ Testing the email
 
 Running the script without the `--send` argument will add `/dry-run` to the `url_fetch_subscribers` and expects to get a list of dummy emails that will be used for testing the resulting email.
 
-Sending the email
+Sending the email with API
 
 `newsletter.py --feed https://WEBSITE.COM/feed --send`
 
-## How it works
+Sending the email with CSV
 
-This script does not manage your subscribers, it expects to fetch the email addresses from an API endpoint at `url_fetch_subscribers` and to send the email with a Mailgun account.
+`newsletter.py --feed https://WEBSITE.COM/feed --csv /path/to/emails.csv --send`
 
-Edit `example.env` to configure your settings.
-
+Remember to edit `example.env` to configure your settings.
 ## Templates
 
 The `templates` directory has two examples on how to organise and format your email with html and a simple substitution logic.
@@ -50,4 +65,3 @@ The `templates` directory has two examples on how to organise and format your em
 Edit this line to change the template used:
 
 `template_file_path = os.path.join(script_dir, 'templates', 'weekly-notes.html')`
-
--- newsletter.py
+++ newsletter.py
@@ -8,31 +8,32 @@ import os
 import requests
 import smtplib
 import hashlib
+import csv
 from dotenv import load_dotenv
 
 def send_email(to_addr, content, subject, from_addr, mailgun_server, mailgun_password):
-    # Create and configure the message
-    message = MIMEMultipart()
-    message['From'] = from_addr
-    message['To'] = to_addr
-    message['Subject'] = subject
-    message.attach(MIMEText(content, 'html'))
+	# Create and configure the message
+	message = MIMEMultipart()
+	message['From'] = from_addr
+	message['To'] = to_addr
+	message['Subject'] = subject
+	message.attach(MIMEText(content, 'html'))
 
-    # Send the email
-    with smtplib.SMTP(mailgun_server, 587) as server:
-        server.starttls()
-        server.login(from_addr, mailgun_password)
-        server.send_message(message)
+	# Send the email
+	with smtplib.SMTP(mailgun_server, 587) as server:
+		server.starttls()
+		server.login(from_addr, mailgun_password)
+		server.send_message(message)
 
 def unsub_link(email):
-    string_to_hash = f'{{"email":"{email}"}}'
-    hash_object = hashlib.sha256()
-    # Update the hash object with the bytes of the input string
-    hash_object.update(string_to_hash.encode())
-    # Get the hexadecimal representation of the hash
-    hash = hash_object.hexdigest()
-    link = f'https://YOUR-WEBSITE.COM/unsubscribe/?e={hash}'
-    return link
+	string_to_hash = f'{{"email":"{email}"}}'
+	hash_object = hashlib.sha256()
+	# Update the hash object with the bytes of the input string
+	hash_object.update(string_to_hash.encode())
+	# Get the hexadecimal representation of the hash
+	hash = hash_object.hexdigest()
+	link = f'https://YOUR-WEBSITE.COM/unsubscribe/?e={hash}'
+	return link
 
 # Load environment variables
 load_dotenv()
@@ -50,16 +51,17 @@ from_addr = f'"{from_name}" <{from_email}>'
 parser = argparse.ArgumentParser(description='Send requests to API.')
 parser.add_argument('--feed', type=str, required=True, help='RSS Feed URL')
 parser.add_argument('--send', action='store_true', help='Use the production URL and send the emails')
+parser.add_argument('--csv', type=str, help='Path to CSV file containing email addresses')
 args = parser.parse_args()
 
 # Fetch the latest feed
 feed = feedparser.parse(args.feed)
 if feed.bozo:
-    print("Error parsing feed:", feed.bozo_exception)
-    sys.exit(1)
+	print("Error parsing feed:", feed.bozo_exception)
+	sys.exit(1)
 if not feed.entries:
-    print("No entries found in the feed.")
-    sys.exit(1)
+	print("No entries found in the feed.")
+	sys.exit(1)
 
 # Retrieve the latest post
 latest_post = feed.entries[0]
@@ -68,7 +70,7 @@ latest_post = feed.entries[0]
 script_dir = os.path.dirname(os.path.realpath(__file__))
 template_file_path = os.path.join(script_dir, 'templates', 'weekly-notes.html')
 with open(template_file_path, 'r', encoding='utf-8') as file:
-    html_template = file.read()
+	html_template = file.read()
 
 # Prepare content
 current_date = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -80,46 +82,49 @@ p_style = "font-family: sans-serif; font-size: 16px; font-weight: normal; margin
 
 # Find all <p> tags and apply the style
 for p_tag in soup.find_all('p'):
-    prev_sibling = p_tag.find_previous_sibling()
-    if prev_sibling and prev_sibling.name == 'p':
-        new_br_tag = soup.new_tag("br")
-        p_tag.insert_before(new_br_tag)
-    p_tag['style'] = p_style
+	prev_sibling = p_tag.find_previous_sibling()
+	if prev_sibling and prev_sibling.name == 'p':
+		new_br_tag = soup.new_tag("br")
+		p_tag.insert_before(new_br_tag)
+	p_tag['style'] = p_style
 
 # Resize images if they are wider than 400px
 for img_tag in soup.find_all('img'):
-    # Set the width to 100% to ensure full column width on all devices
-    img_tag['style'] = "width: 100%; height: auto;"
-    # Remove width and height attributes to prevent conflicts
-    img_tag.attrs.pop('width', None)
-    img_tag.attrs.pop('height', None)
+	# Set the width to 100% to ensure full column width on all devices
+	img_tag['style'] = "width: 100%; height: auto;"
+	# Remove width and height attributes to prevent conflicts
+	img_tag.attrs.pop('width', None)
+	img_tag.attrs.pop('height', None)
 
 # Update the content with modified HTML
 content = str(soup)
 
-# Example of how you might extract the first image URL from the content
-# This is a simplified example; you might need a more robust method depending on your HTML structure
-# soup = BeautifulSoup(content, 'html.parser')
-# first_image = soup.img['src'] if soup.img else ''
-
 # Replace placeholders in the template
 email_content = html_template.replace("{{date}}", current_date)
-email_content = email_content.replace("{{permalink}}",latest_post.link)
+email_content = email_content.replace("{{permalink}}", latest_post.link)
 email_content = email_content.replace("{{title}}", title)
 email_content = email_content.replace("{{content}}", content)
 
 # Fetch email addresses
-# Determine the URL where to get the list of emails and authenticate with bearer token
-url = os.getenv('URL_FETCH_SUBSCRIBERS')
-if not args.send:
-    url += '/dry-run'
-headers = { 'Authorization': f'Bearer {subscribers_bearer_token}' }
-response = requests.post(url, headers=headers)
-email_addresses = response.text.split(', ')
+if args.csv:
+	# Load email addresses from the CSV file
+	email_addresses = []
+	with open(args.csv, newline='') as csvfile:
+		reader = csv.reader(csvfile)
+		for row in reader:
+			email_addresses.append(row[0])
+else:
+	# Determine the URL where to get the list of emails and authenticate with bearer token
+	url = os.getenv('URL_FETCH_SUBSCRIBERS')
+	if not args.send:
+		url += '/dry-run'
+	headers = { 'Authorization': f'Bearer {subscribers_bearer_token}' }
+	response = requests.post(url, headers=headers)
+	email_addresses = response.text.split(', ')
 
 # Send emails
 for to_addr in email_addresses:
-    unsubscribe_link = unsub_link(to_addr)
-    individual_content = email_content.replace("{{unsubscribe_link}}", unsubscribe_link)
-    send_email(to_addr, individual_content, latest_post.title, from_addr, mailgun_server, mailgun_password)
-    print(f"Email sent successfully to {to_addr}!")
+	unsubscribe_link = unsub_link(to_addr)
+	individual_content = email_content.replace("{{unsubscribe_link}}", unsubscribe_link)
+	send_email(to_addr, individual_content, latest_post.title, from_addr, mailgun_server, mailgun_password)
+	print(f"Email sent successfully to {to_addr}!")
